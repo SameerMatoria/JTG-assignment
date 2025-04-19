65fa55186c1b361fa5db1d75ba9edc29909729e8
SameerMatoria <sameermatoria@gmail.com>
2025-04-19 17:31:18 +0530

    done till maps

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("hi");
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('header');
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
