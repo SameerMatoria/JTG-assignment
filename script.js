b27b4fa656963feb5f83da7ae2a630d8134ccca4
SameerMatoria <sameermatoria@gmail.com>
2025-04-23 01:06:50 +0530

    final changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 window.addEventListener('scroll', function () {
-    const header = document.getElementById('header');
-    if (window.scrollY > 0) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
+  const header = document.getElementById('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
